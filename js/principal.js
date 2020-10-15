3f571cd937dd9c23293f44a5e2a5382ae0961902
janainacarilaine <janainacarilaine@gmail.com>
2020-10-15 11:04:16 -0300

    adicionado formulario para inclusao de clientes e logica js para tal

--- js/principal.js
+++ js/principal.js
@@ -1,9 +1,8 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent= "Aparecida Nutricionista";
 
-//var trPaciente = document.querySelectorAll(".paciente");
 var pacientes = document.querySelectorAll(".paciente"); //pega todos os elementos da classe paciente
-//console.log(pacientes);
+
 for(var i=0;i<pacientes.length;i++){
     var paciente = pacientes[i]; 
     var trPeso = paciente.querySelector(".info-peso");
@@ -18,7 +17,6 @@ for(var i=0;i<pacientes.length;i++){
     var alturaValida= true;
 
     if (peso<=0 || peso >=1000){
-        console.log("Peso inválido!");
         tdImc.textContent = "Peso inválido";
         pesoValido=false;
         paciente.classList.add("paciente-invalido");
@@ -35,7 +33,45 @@ for(var i=0;i<pacientes.length;i++){
     }else{
         tdImc.textContent= "Altura e/ou peso inválidos!"
     }
+
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente"); //traz o botao de adicionar paciente
+botaoAdicionar.addEventListener("click", function (event){ //add um escutador de evento, no caso o click do botao adicionar 
+    event.preventDefault(); // previne o comportamento padrão da página (salvar, limpar e recarregar)
+
+    var form = document.querySelector("#form-adiciona"); //traz o form do html para ser manipulado no js
+    // usa o name do input para guardar o valor das entradas
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr"); //cria o paciente e informa que e um tr
+
+   //aqui cria todos os elementos do paciente e informa que sao as td's
+   var nomeTd = document.createElement("td");
+   var pesoTd = document.createElement("td");
+   var alturaTd = document.createElement("td");
+   var gorduraTd = document.createElement("td");
+   var imcTd = document.createElement("td");
+
+   //adiciona as entradas nas td's criadas
+   nomeTd.textContent = nome;
+   pesoTd.textContent = peso;
+   alturaTd.textContent = altura;
+   gorduraTd.textContent = gordura;
+
+   //adiciona como filho (td's dentro da tr) / atributos de paciente agora estao associados como parte dele
+   pacienteTr.appendChild(nomeTd);
+   pacienteTr.appendChild(pesoTd);
+   pacienteTr.appendChild(alturaTd);
+   pacienteTr.appendChild(gorduraTd);
+   
+   var tabela = document.querySelector("#tabela-pacientes"); //busca a tabela de pacientes do html para ser manipulado
+   tabela.appendChild(pacienteTr); //adiciona o paciente como filho / parte da tabela 
+});
+
 
  
  
